d974b9c76d6e336ab16f2ee1e94210f54f5aa6b1
RuchaS2 <88849908+RuchaS2@users.noreply.github.com>
2021-08-19 10:58:00 +0530

    Delete read_files.scala

--- src/main/scala/read_files.scala
+++
@@ -1,9 +0,0 @@
-import scala.io.Source._
-object read_files extends App{
-  val filename = "src/rainbow1.txt"
-  for(ine <- fromFile(filename).getLines()){
-    println(ine.toUpperCase())
-  }
-  val poemlines = fromFile(filename).getLines().toList
-  println(poemlines)
-}
